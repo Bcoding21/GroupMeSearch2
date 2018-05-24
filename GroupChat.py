f11aaef61676d3c661915e5c3e3a78de040a675c
Brandon <brandon.cole@bison.howard.edu>
2018-05-24 11:30:15 -0400

    Created GroupChat class and constructor

---
+++ GroupChat.py
@@ -0,0 +1,13 @@
+
+class GroupChat(object):
+
+    def __init__(self):
+        self.group_name = ""
+        self.group_id = -1
+
+    def find_messages_any(self, words):
+        pass
+
+    def find_messages_all(self, words):
+        pass
+
