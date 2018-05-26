102bb855df05cda5392d4cc33c2b8685f8dcba11
Bcoding21 <brandoncole673@gmail.com>
2018-05-26 13:43:32 -0400

    Added two functions to search for keywords in messages

--- GroupChat.py
+++ GroupChat.py
@@ -1,13 +1,19 @@
 
 class GroupChat(object):
 
-    def __init__(self):
-        self.group_name = ""
-        self.group_id = -1
+    def __init__(self, json):
+        self.__name = json[response]["name"]
+        self.__id = json[response]["id"]
+        self.__messages = []
+        self.__members = []
 
-    def find_messages_any(self, words):
-        pass
+    def find_messages_any(self, key_words):
+        return [message for message in __messages
+        if any(word in message for word in key_words)]
 
     def find_messages_all(self, words):
-        pass
+        return [messages for message in __messages
+        if all(word in message for word in key_words)]
 
+    def __get_variations(self, key_words):
+        pass
