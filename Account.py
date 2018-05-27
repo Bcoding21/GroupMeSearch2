07cc24166c138046b8f324360520d65b2515c37f
Bcoding21 <brandoncole673@gmail.com>
2018-05-26 22:17:43 -0400

    Added function to load all groupchats

--- Account.py
+++ Account.py
@@ -1,11 +1,26 @@
+from pip._vendor import requests
+from GroupChat import GroupChat
+
+
 class Account(object):
 
     base_url = "https://api.groupme.com/v3"
-    group_url = "https://api.groupme.com/v3/groups"
+    groups_url = "https://api.groupme.com/v3/groups"
     messages_url = "https://api.groupme.com/v3/messages"
+    per_page_param = "&per_page="
+    token_param = "?token="
 
-    def __init(self):
-        self.__access_code = ""
+    def __init__(self, access_token):
+        self.token = access_token
+        self.group_chats = self.load_chats()
 
-    def __init__(self, access_code):
-        self.__access_code = access_code
+    def __get_groups_url(self, per_page=100):
+        return self.groups_url + self.token_param + self.token + \
+                self.per_page_param + str(per_page)
+
+    def load_chats(self):
+        url = self.__get_groups_url()
+        response = requests.get(url)
+        if response.status_code == 200:
+            data = response.json()
+            return map(GroupChat, data['response'])
